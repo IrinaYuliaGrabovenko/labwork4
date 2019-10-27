01b3b8b64dad30cec08eb65937047c4261cb5840
Julia8256 <julia.grabovenko2@gmail.com>
2019-10-27 21:01:11 +0200

    lab4 commited

---
+++ Makefile
@@ -0,0 +1,9 @@
+all:
+	g++ main.cpp factorial.cpp factorial.h -o main
+
+
+clean:
+	rm main
+
+
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "factorial.h"
+
+using namespace std;
+
+int main(){
+    cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
